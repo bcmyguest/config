27675d7d23eb9a22722727ffe9fcfcecc28b0d34
Brandon Guest <bguest95@gmail.com>
2026-02-07 09:33:58 -0500

    fix: keybind

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -20,7 +20,7 @@ return {
 			function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
 		vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
 			{ desc = "Execute opencode action…" })
-		vim.keymap.set({ "n", "t" }, "<C-,>", function() require("opencode").toggle() end,
+		vim.keymap.set({ "n", "t" }, "<M-,>", function() require("opencode").toggle() end,
 			{ desc = "Toggle opencode" })
 
 		vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
